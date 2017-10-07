04d18348847ea6c42ad44f1d87da585c9f29b251
Unknown <coombes.jd@gmail.com>
2017-10-07 23:19:30 +0100

    make not read-only mongoDB

--- settings.py
+++ settings.py
@@ -59,3 +59,12 @@ DOMAIN =\
             }
         }
     }
+
+# Enable reads (GET), inserts (POST) and DELETE for resources/collections
+# (if you omit this line, the API will default to ['GET'] and provide
+# read-only access to the endpoint).
+RESOURCE_METHODS = ['GET', 'POST', 'DELETE']
+
+# Enable reads (GET), edits (PATCH), replacements (PUT) and deletes of
+# individual items  (defaults to read-only item access).
+ITEM_METHODS = ['GET', 'PATCH', 'PUT', 'DELETE']
