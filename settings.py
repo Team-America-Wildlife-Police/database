7b9b1ba4caa1f4bf56f8afde43b05e3ebbf5a59f
Unknown <coombes.jd@gmail.com>
2017-10-07 22:19:16 +0100

    Created MongoDB schema

--- settings.py
+++ settings.py
@@ -6,45 +6,52 @@ Created on Sat Oct  7 15:21:03 2017
 """
 
 DOMAIN =\
-    { "posts":
+    { "post":
         { "schema": 
-            { "title": { "type": "string" }
+            { # scraped fields
+            , "title": { "type": "string" }
             , "username": { "type": "string" }
-            , "uuid": {"type":"string"}
-            , "source": { "type": "string" }
             , "url": {"type":"string"}
-            , "date": {"type":"datetime"}
-            , "location": { "type": "string" }
+            , "date_listed": {"type":"datetime"}
+            , "date_accessed": {"type":"datetime"}
+            , "post_geolocation": { "type": "string" }
+            , "reported_animal" : {"type": "string"}
+            , "reported_how": {}
+            , "reported_where": {}
+            , "source": [ "Ebay"
+                        , "Gumtree"
+                        , "Twitter"
+                        ]
+            
             , "description": {}
+            , "keyword tags": []
             , "price": {}
+            , "quantity": {}
+            , "species": {}
+            , "unit": {} #kg, individuals, etc.
+            , "url": {}
+            , "username": {}
+            
+              # user - entered fields
+            , "additional_notes": {}
+            , "class": {}
+            , "common_names": {}
+            , "case_handler_contact_info": {}
+            , "exporter_country": {}
+            , "family": {}
+            , "genus": {}
+            , "importer_country": {}
+            , "listing_type": 
+                [ "Import request"
+                , "Export offer"
+                , "Commision"
+                ]
+            , "order": {}
+            , "purpose": {} #Trophy, medicine, taxidermy
+            , "relevant_authorities": {}
+            , "taxon": {}
+            , "trader": {} #person
+            , "verified by": {} #e.g. sergio.
             }
-        }
-    , "accounts": 
-        { "schema": 
-            { "site": { "type": "string" }
-            , "username": { "type": "string" }
-            }
-        }
-    , "traders": 
-        { "schema": 
-            { "accounts": 
-                { "type": "list"
-                , "schema": 
-                    { "type": "objectid"
-                    , "data_relation": 
-                        { "resource": "accounts"
-                        , "field": "_id"
-                        , "embeddable": True
-                        }
-                    }
-                }
-            }
-        }
-    , "taxon":
-        { "schema":
-            { 
-            , 
-             }
         }
     }
-}
